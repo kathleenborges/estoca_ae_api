b7dfca4bcb93923d3ce165f2c52c8d52c624f22d
kathleenborges <kathleenborges@outlook.com>
2025-12-21 13:25:08 -0300

    Add data_cadastro field to cadastro schema

--- schemas/cadastro.py
+++ schemas/cadastro.py
@@ -14,6 +14,7 @@ class RespostaCadastroSchema(BaseModel):
     nome: str = "Sapato"
     valor: float = 100.00
     link: str = "https://www.google.com"
+    data_cadastro: str 
 
     model_config = ConfigDict(from_attributes=True)
 
